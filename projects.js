1101c2fa1f4dae9f763852e18d251d3a1c1a3253
LittleVoid <08evangelion08@gmail.com>
2025-04-10 16:41:34 +0200

    [+} Added projects.js for a List of Projects and there Data

---
+++ projects.js
@@ -0,0 +1,26 @@
+const projects = [
+    {
+        title: "Project_01",
+        videoId: "sdahgjv", // Only YT Video ID
+        description: "This is fun game",
+        tags: ["Unity", "C#", "Level Design"]
+    },
+    {
+        title: "Project_02",
+        videoId: "sdahgjv", // Only YT Video ID
+        description: "This is fun game",
+        tags: ["Unity", "C#", "Level Design"]
+    },
+    {
+        title: "Project_03",
+        videoId: "sdahgjv", // Only YT Video ID
+        description: "This is fun game",
+        tags: ["Unity", "C#", "Level Design"]
+    },
+    {
+        title: "Project_04",
+        videoId: "sdahgjv", // Only YT Video ID
+        description: "This is fun game",
+        tags: ["Unity", "C#", "Level Design"]
+    },
+]
