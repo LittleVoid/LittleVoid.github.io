b25266fa20cf9065422aa36cda7710b21f28fd83
LittleVoid <08evangelion08@gmail.com>
2025-04-12 20:44:52 +0200

    [*] Updated Project-Card Layout Frontend

--- projects.js
+++ projects.js
@@ -1,9 +1,20 @@
+/*  example:
+        title: "Nautilus",
+        genre: "Tactical Shooter",
+        role: "Design Lead",
+        roleDescription: "Led the level design and balancing of combat encounters.",
+        videoId: "jWcOfbf_yOU",
+        tags: ["Unreal Engine", "Blueprints", "Level Design"],
+        playLink: "https://your-itch-link.com",
+        moreLink: "project-pages/kyoto-conflict.html"
+*/
+
 const projects = [
     {
         title: "Kyoto Conflict",
         genre: "Tactical Shooter",
         role: "Design Lead",
-        roleDescription: "Led the level design and balancing of combat encounters.",
+        roleDescription: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
         videoId: "jWcOfbf_yOU",
         tags: ["Unreal Engine", "Blueprints", "Level Design"],
         playLink: "https://your-itch-link.com",
